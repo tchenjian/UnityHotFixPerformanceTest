d60d89a5eda037ae770783568e0f2abfaefe2205
tchenjian <tchenjiant@163.com>
2023-03-26 14:10:23 +0800

    xlua 测试代码

---
+++ 14_Performance/PerformanceTest.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using XLua;
+using UnityEngine.UI;
+
+namespace XLuaTest
+{
+    public class PerformanceTest : MonoBehaviour
+    {
+        LuaEnv luaenv = null;
+        public Text output;
+        public Button lua;
+        // Use this for initialization
+        void Start()
+        {
+            Application.logMessageReceived+=HandleLog;
+            lua.onClick.AddListener(StartLuaTest);
+        }
+
+        private void StartLuaTest()
+        {
+            luaenv = new LuaEnv();
+            luaenv.DoString("require 'Performance'");
+        }
+
+        void HandleLog(string logString, string stackTrace, LogType type)
+        {
+            output.text = output.text += "\n"+logString;
+        }
+
+        void OnDestroy()
+        {
+            luaenv.Dispose();
+        }
+    }
+}
---
+++ xlua_Test_Init.bat
@@ -0,0 +1,2 @@
+xcopy .\14_Performance .\xLua\Assets\XLua\Examples\14_Performance\ /e /y /h /r /q
+pause
